1dc959fcc920b34c97d6a4878871c75ba979802b
prodsadz <115144204+adamurbanek@users.noreply.github.com>
2025-03-26 11:37:29 +0100

    umru

--- stinsily/stinsily.client/src/config/api.ts
+++ stinsily/stinsily.client/src/config/api.ts
@@ -2,6 +2,6 @@ export const API_BASE_URL = 'https://id-46.pslib.cloud';
 
 export const getImageUrl = (imageUrl: string) => {
   if (!imageUrl) return '';
-  const baseUrl = 'https://id-46.pslib.cloud';
+  const baseUrl = 'https://id-46.pslib.cloud/api';
   return `${baseUrl}${imageUrl}`;
 };
