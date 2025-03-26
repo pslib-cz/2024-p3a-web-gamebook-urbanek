5aa338a7e88afdf41933f74666d372e2825cfb35
prodsadz <115144204+adamurbanek@users.noreply.github.com>
2025-03-26 11:16:17 +0100

    pls

--- stinsily/stinsily.client/src/Components/Scene.tsx
+++ stinsily/stinsily.client/src/Components/Scene.tsx
@@ -101,7 +101,7 @@ const Scene = () => {
             setCurrentScene(sceneData);
 
             // Fetch options for this scene
-            const optionsResponse = await fetch(`${API_BASE_URL}/scenes/options/${sceneId}`, {
+            const optionsResponse = await fetch(`${API_BASE_URL}/scene/options/${sceneId}`, {
                 method: 'GET',
                 credentials: 'include',
                 headers: {
@@ -330,7 +330,7 @@ const Scene = () => {
             }
 
             // Get the current scene options again
-            const sceneOptionsResponse = await fetch(`${API_BASE_URL}/scenes/options/${id}`, {
+            const sceneOptionsResponse = await fetch(`${API_BASE_URL}/scene/options/${id}`, {
                 headers: {
                     'Authorization': `Bearer ${token}`
                 }
