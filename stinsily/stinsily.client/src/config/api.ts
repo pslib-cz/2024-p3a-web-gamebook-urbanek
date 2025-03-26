91dd00d0bae9eaaaf56077a30a42e2e344059d6d
prodsadz <115144204+adamurbanek@users.noreply.github.com>
2025-03-26 12:14:11 +0100

    Revert "sdasdf"
    This reverts commit 70513b634d0cef205d2b14d05cbed2bc860c5302.

--- stinsily/stinsily.client/src/config/api.ts
+++ stinsily/stinsily.client/src/config/api.ts
@@ -2,6 +2,6 @@ export const API_BASE_URL = 'https://id-46.pslib.cloud';
 
 export const getImageUrl = (imageUrl: string) => {
   if (!imageUrl) return '';
-  const baseUrl = 'https://id-46.pslib.cloud' : 'http://localhost:5193';
+  const baseUrl = import.meta.env.PROD ? 'https://id-46.pslib.cloud' : 'http://localhost:5193';
   return `${baseUrl}${imageUrl}`;
 };
